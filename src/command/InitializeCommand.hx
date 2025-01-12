a7245cc4b9ee4be694a161219bf8c9e38eb9d0bb
Barış Yıldırım <fb_bcy@hotmail.com>
2025-01-12 20:47:30 +0300

    update

--- src/command/InitializeCommand.hx
+++ src/command/InitializeCommand.hx
@@ -18,14 +18,6 @@ class InitializeCommand extends Command {
             fail();
         }
 
-        trace("sceUserServiceInitialize success");
-        trace(Systemctl.readStringByName("net.inet.ifq.ifq_len"));
-        trace("1");
-
-        trace(LibSceUserService.getLoginUserIdList());
-
-        LibSceSystemService.lncUtilLaunchApp("NPXS40093");
-
         #end
     }
 }
