a1e958f8cefc1291fe01a7018b9d271f235542fc
Barış Yıldırım <fb_bcy@hotmail.com>
2025-01-23 18:47:58 +0300

    dependency installer script added

---
+++ install.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Install Haxe
+apt update
+apt install sudo software-properties-common -y
+sudo add-apt-repository ppa:haxe/releases -y
+sudo apt-get update
+sudo apt-get install haxe -y
+mkdir ~/haxelib && haxelib setup ~/haxelib
+
+# Install SDK dependencies
+apt install bash clang-15 lld-15 socat cmake meson pkg-config wget unzip git -y
+
+# Install SDK
+DEFAULT_SDK_PATH="/opt/ps5-payload-sdk"
+installSDK="true"
+if [[ -d "$DEFAULT_SDK_PATH" ]]; then
+  installSDK="null"
+  while [ "$installSDK" == "null" ]
+  do
+     echo "It looks like you seem to have the PS5 SDK installed, would you like to refresh the installation? (y/n)"
+     read wantRefreshSDK
+
+     if [ "$wantRefreshSDK" == "y" ]; then
+       installSDK="true"
+     elif [ "$wantRefreshSDK" == "n" ]; then
+       installSDK="false"
+     else
+       echo "Invalid input, please try again."
+     fi
+  done
+else
+  echo "SDK is not installed."
+fi
+
+if [[ "$installSDK" == "true" ]]; then
+    # Remove SDK
+    rm -r $DEFAULT_SDK_PATH
+
+    # Install Latest SDK
+    wget -P /opt https://github.com/ps5-payload-dev/sdk/releases/latest/download/ps5-payload-sdk.zip
+    sudo unzip -d /opt /opt/ps5-payload-sdk.zip
+fi
+
+# Install Haxe Libraries
+haxelib install haxe-concurrent 5.1.4
+haxelib install hscript 2.6.0
+haxelib install rulescript 0.2.0
+
+# Install HXCPP
+mkdir -p ~/haxelib/hxcpp
+if [ ! -d "$HOME/haxelib/hxcpp/ps5-payload" ]; then
+  git clone https://github.com/barisyild/hxcpp/ --branch ps5-payload ~/haxelib/hxcpp/ps5-payload
+  echo ps5-payload > ~/haxelib/hxcpp/.current
+else
+  cd ~/haxelib/hxcpp/ps5-payload/ && git pull
+fi
+cd ~/haxelib/hxcpp/ps5-payload/tools/hxcpp/ && haxe compile.hxml
