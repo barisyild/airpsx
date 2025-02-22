69b552e8c7eedf0a54e0ab79cd206c3a0b263d85
Barış Yıldırım <fb_bcy@hotmail.com>
2025-02-22 13:31:57 +0300

    update version

--- src/AfterBuild.hx
+++ src/AfterBuild.hx
@@ -60,7 +60,7 @@ class AfterBuild {
 
         var elfFilename = "airpsx.elf";
         var tid = "AIRX00000";
-        var version = "0.04";
+        var version = "0.05";
 
         if (isValidVersion(version) && isValidTid(tid)) {
             addHeaderToElf(elfFilename, tid, version);
