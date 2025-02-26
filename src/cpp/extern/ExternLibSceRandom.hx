f6f018cd32d1b5c61c9fc5339f54c6bbf0ae6966
Barış Yıldırım <fb_bcy@hotmail.com>
2025-02-26 21:14:27 +0300

    improve code

--- src/cpp/extern/ExternLibSceRandom.hx
+++ src/cpp/extern/ExternLibSceRandom.hx
@@ -1,18 +1,8 @@
 package cpp.extern;
 
 #if orbis
-import cpp.AbstractPointer;
-import cpp.CastCharStar;
-import cpp.ConstCharStar;
 import cpp.Int32;
-import cpp.Int32Pointer;
-import cpp.Int64;
-import cpp.PidT;
 import cpp.SizeT;
-import cpp.UInt32;
-import cpp.UInt64;
-import cpp.Pointer;
-import cpp.UInt8;
 import cpp.AbstractArrayPointer;
 
 @:keep
