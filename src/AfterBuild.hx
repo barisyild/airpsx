01d6f8df436e62a8902e872180faf4e99e51b36e
barisyild <fb_bcy@hotmail.com>
2025-05-16 18:19:28 +0300

    update build script

--- src/AfterBuild.hx
+++ src/AfterBuild.hx
@@ -1,10 +1,20 @@
 package ;
 
 import sys.FileSystem;
+import sys.io.File;
+import sys.io.FileOutput;
 using StringTools;
 
 class AfterBuild {
     static function main():Void {
-        FileSystem.rename("out/HxWell", "airpsx.elf");
+        FileSystem.rename("prospero/HxWell", "airpsx.elf");
+
+        var elfFile = File.read("airpsx.elf", true);
+
+        var newElfFilePath:String = "websrv_template/airpsx.elf";
+        var newElfFile:FileOutput = File.write(newElfFilePath, true);
+        newElfFile.writeInput(elfFile);
+        elfFile.close();
+        newElfFile.close();
     }
 }
