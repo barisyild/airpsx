1aac9b0a70a78ffce28b75cfed75d4e87a030a7e
barisyild <fb_bcy@hotmail.com>
2025-06-03 17:27:38 +0300

    update random

--- src/cpp/lib/LibSceRandom.hx
+++ src/cpp/lib/LibSceRandom.hx
@@ -5,9 +5,10 @@ import cpp.extern.ExternLibSceRandom;
 
 class LibSceRandom {
     @:hscriptVariable("sceRandomGetRandomNumber")
-    public static function getRandomNumber(array:Array<cpp.UInt8>):Void
+    public static function getRandomNumber(array:Array<cpp.UInt8>):Array<cpp.UInt8>
     {
         ExternLibSceRandom.sceRandomGetRandomNumber(array, array.length);
+        return array;
     }
 }
 #end
