d16f835f5dbba93b4f011659ccaaeb00f1f6440a
barisyild <fb_bcy@hotmail.com>
2025-08-31 02:29:51 +0300

    make `SCRIPT_DB_PATH` inline in Const for consistency

--- src/airpsx/Const.hx
+++ src/airpsx/Const.hx
@@ -16,7 +16,7 @@ class Const {
     public static inline var HTTP_PORT:Int = 1214;
     public static inline var SYSTEM_APP_DB_PATH = "/system_data/priv/mms/app.db";
     public static inline var SYSTEM_APP_INFO_DB_PATH = "/system_data/priv/mms/appinfo.db";
-    public static var SCRIPT_DB_PATH = '${SCRIPT_PATH}/database.db';
+    public static inline var SCRIPT_DB_PATH = '${SCRIPT_PATH}/database.db';
     public static inline var USER_HOME_PATH:String = "/user/home";
 
     public static var storageDirectories:Map<String, String> = new Map<String, String>();
