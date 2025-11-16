a1507dbf0db38c47dc2f58341fcc4af29bc43d35
barisyild <fb_bcy@hotmail.com>
2025-11-16 20:18:37 +0300

    add conditional compilation directives to constants in Const.hx

--- src/airpsx/Const.hx
+++ src/airpsx/Const.hx
@@ -13,10 +13,10 @@ class Const {
     public static #if orbis inline #end var DB_PATH:String = '${DATA_PATH}/app.db';
     public static #if orbis inline #end var TASK_DB_PATH:String = '${DATA_PATH}/task.db';
     public static #if orbis inline #end var SCRIPT_PATH:String = '${DATA_PATH}/scripts';
-    public static inline var HTTP_PORT:Int = 1214;
-    public static inline var SYSTEM_APP_DB_PATH = "/system_data/priv/mms/app.db";
-    public static inline var SCRIPT_DB_PATH = '${SCRIPT_PATH}/database.db';
-    public static inline var USER_HOME_PATH:String = "/user/home";
+    public static #if orbis inline #end var HTTP_PORT:Int = 1214;
+    public static #if orbis inline #end var SYSTEM_APP_DB_PATH = "/system_data/priv/mms/app.db";
+    public static #if orbis inline #end var SCRIPT_DB_PATH = '${SCRIPT_PATH}/database.db';
+    public static #if orbis inline #end var USER_HOME_PATH:String = "/user/home";
 
     public static var storageDirectories:Map<String, String> = new Map<String, String>();
 
