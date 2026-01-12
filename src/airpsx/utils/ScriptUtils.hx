579b0b7d2ad53865174b4160e3737e0a7242b296
barisyild <fb_bcy@hotmail.com>
2026-01-12 19:39:25 +0300

    Disable lua backend for orbis

--- src/airpsx/utils/ScriptUtils.hx
+++ src/airpsx/utils/ScriptUtils.hx
@@ -3,14 +3,17 @@ import haxe.io.Output;
 import airpsx.type.ScriptType;
 import airpsx.script.RuleScriptBackend;
 import airpsx.script.ScriptBackend;
-import airpsx.script.LuaScriptBackend;
 class ScriptUtils {
     public static function execute(script:String, type:ScriptType, output:Output, ?extra:Map<String, Any>):Dynamic {
         var scriptBackend:ScriptBackend = switch (type) {
             case RULESCRIPT:
                 new RuleScriptBackend();
             case LUA:
-                new LuaScriptBackend();
+                #if prospero
+                new airpsx.script.LuaScriptBackend();
+                #else
+                throw "LuaScriptBackend not implemented";
+                #end
         }
 
         scriptBackend.output = output;
